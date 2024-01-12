c895edc49dad37e94ea8c3519a86eafc33578c8d
Anthority <leyun_tian@123.com>
2024-01-12 11:37:41 +0800

    change gitinnore and rm config.hpp

--- OpenTimer/ot/config.hpp
+++
@@ -1,8 +0,0 @@
-#define OT_VERSION_MAJOR 2
-#define OT_VERSION_MINOR 1
-#define OT_VERSION_PATCH 0
-#define OT_VERSION "2.1.0"
-#define OT_HOME "/home/tianly/OpenTimer/OpenTimer"
-#define OT_HOST_SYSTEM "Linux-5.15.0-89-generic"
-#define OT_INSTALL_PREFIX "/usr/local"
-#define OT_TCLSH_PATH "/bin/tclsh"
