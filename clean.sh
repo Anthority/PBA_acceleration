5d49af40a8ef89bcb17ac90ff7e4e4cc8882b2e6
Anthority <leyun_tian@126.com>
2024-03-13 14:31:53 +0800

    add automatic scripts

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+cd $1
+rm -f $1.exe
+cd ..
+
--- cmp_run.sh
+++ cmp_run.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/bash
-cd $1
-cd cmake-build-debug
-cmake ..
+
+# Remember the first argument is the benchmark path
+
+FILE=$1/$2/main.cpp
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else
+    echo "$FILE does not exist."
+    cp Doc/main_template.cpp $1/$2/main.cpp
+fi
+
+cd build
+cmake .. -DBENCHMARK:STRING=$1 -DCASE:STRING=$2
 make -j 8
 cd ..
-./$1 $1 $2 $3 > test.log
+cd $1/$2/
+./$2.exe $2 $3 $4 > test.log
 cd ..
